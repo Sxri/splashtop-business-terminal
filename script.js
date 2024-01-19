412d1d2a3ce9fa50c5beb1db60235680b52a75ad
lurk <84330709+Sxri@users.noreply.github.com>
2024-01-19 10:13:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const output = document.getElementById('output');
   const input = document.getElementById('input');
 
   output.innerHTML += "Welcome to the Splashtop Business Terminal. A terminal where you can find functional resources and utilities.\nThis website is intended specifically for the Splashtop Business app on ChromeOS, say `help` for a list of commands. \n\n";
   output.innerHTML += '<span style="font-weight: bold; color: red;">[COMMANDS ARE IN TESTING STAGE. BUGS WILL OCCUR]</span>\n\n';
 
-  input.addEventListener('keydown', function(event) {
+  input.addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
       event.preventDefault();
       processCommand(input.value);
